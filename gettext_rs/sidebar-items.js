58a803a670505f5401e91bcccb51138d76f2f144
Konstantin Salikhov <koka58@yandex.ru>
2016-02-02 12:21:17 +0400

    First pages commit

---
+++ gettext_rs/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["LocaleCategory","Locale category enum ported from locale.h"]],"fn":[["bindtextdomain","Bind text domain to some directory containing gettext MO files"],["gettext","Translate msgid to localized message"],["setlocale","Set current locale for translations"],["textdomain","Switch to specific text domain"]]});
