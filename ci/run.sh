8fbbfcc6f87200114fdece7e43ce28e6a7a04db4
Brian Olsen <brian@maven-group.org>
2017-12-12 21:57:46 +0100

    Build our own gettext on linux and test with that

--- ci/run.sh
+++ ci/run.sh
@@ -4,6 +4,11 @@ set -ex
 
 cargo test --target $TARGET --no-run -vv
 if [ -z "$NO_RUN" ]; then
+    if [ -n "$GETTEXT_LIB_DIR" ]; then
+      export LD_LIBRARY_PATH=${GETTEXT_LIB_DIR}:$LD_LIBRARY_PATH
+    elif [ -n "$GETTEXT_DIR" ]; then
+      export LD_LIBRARY_PATH=${GETTEXT_DIR}/lib:$LD_LIBRARY_PATH
+    fi
     cargo test --target $TARGET --verbose -- --nocapture
     cargo run --manifest-path systest/Cargo.toml --target $TARGET -vv
     cargo doc --no-deps --target $TARGET
