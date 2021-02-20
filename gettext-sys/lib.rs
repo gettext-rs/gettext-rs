a8d89de6972308158a138e8d79f2c8d8b8aa9e48
Alexander Batischev <eual.jp@gmail.com>
2021-02-20 21:20:18 +0300

    Use wbindtextdomain on Windows
    Fixes #38.

--- gettext-sys/lib.rs
+++ gettext-sys/lib.rs
@@ -1,4 +1,6 @@
 use std::os::raw::{c_char, c_int, c_ulong};
+#[cfg(windows)]
+use std::os::raw::wchar_t;
 
 extern "C" {
     pub fn gettext(s: *const c_char) -> *mut c_char;
@@ -10,6 +12,9 @@ extern "C" {
     pub fn dcngettext(domain: *const c_char, s1: *const c_char, s2: *const c_char, n: c_ulong, category: c_int) -> *mut c_char;
 
     pub fn bindtextdomain(domain: *const c_char, dir: *const c_char) -> *mut c_char;
+    #[cfg(windows)]
+    pub fn wbindtextdomain(domain: *const c_char, dir: *const wchar_t) -> *mut wchar_t;
+
     pub fn textdomain(domain: *const c_char) -> *mut c_char;
 
     pub fn bind_textdomain_codeset(domain: *const c_char, codeset: *const c_char) -> *mut c_char;
