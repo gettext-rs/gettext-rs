df85ef7c5f5f13c26176e5d1e993ccd6918519a8
Alexander Batischev <eual.jp@gmail.com>
2021-03-03 00:39:42 +0300

    Simplify and heavily comment the usage example

--- gettext-rs/src/lib.rs
+++ gettext-rs/src/lib.rs
@@ -6,35 +6,38 @@
 //! use gettextrs::*;
 //!
 //! fn main() -> Result<(), Box<dyn std::error::Error>> {
+//!     // Specify the name of the .mo file to use.
 //!     textdomain("hellorust")?;
-//!     bindtextdomain("hellorust", "/usr/local/share/locale")?;
-//!
-//!     // It's sufficient to call any one of those two. See "UTF-8 is required" section below.
-//!     setlocale(LocaleCategory::LcAll, "en_US.UTF-8");
+//!     // Ask gettext for UTF-8 strings. THIS CRATE CAN'T HANDLE NON-UTF-8 DATA!
 //!     bind_textdomain_codeset("hellorust", "UTF-8")?;
 //!
+//!     // You could also use `TextDomain` builder which calls `textdomain` and
+//!     // other functions for you:
+//!     //
+//!     // TextDomain::new("hellorust").init().unwrap();
+//!
+//!     // `gettext()` simultaneously marks a string for translation and translates
+//!     // it at runtime.
 //!     println!("Translated: {}", gettext("Hello, world!"));
+//!
+//!     // gettext supports plurals, i.e. you can have different messages depending
+//!     // on the number of items the message mentions. This even works for
+//!     // languages that have more than one plural form, like Russian or Czech.
 //!     println!("Singular: {}", ngettext("One thing", "Multiple things", 1));
 //!     println!("Plural: {}", ngettext("One thing", "Multiple things", 2));
-//!     println!("With placeholder: {}", gettext!("Hello, {}!",  "Example User"));
+//!
+//!     // gettext de-duplicates strings, i.e. the same string used multiple times
+//!     // will have a single entry in the PO and MO files. However, the same words
+//!     // might have different meaning depending on the context. To distinguish
+//!     // between different contexts, gettext accepts an additional string:
+//!     println!("With context: {}", pgettext("This is the context", "Hello, world!"));
+//!     println!(
+//!         "Plural with context: {}",
+//!         npgettext("This is the context", "One thing", "Multiple things", 2));
 //!
 //!     Ok(())
 //! }
 //! ```
-//!
-//! Alternatively, you can initialize the locale and text domain using the [`TextDomain`] builder.
-//! By default, a translation of the specified text domain in current language is searched in
-//! the system's data paths. See [`TextDomain`]'s documentation for other options.
-//!
-//! ```rust,no_run
-//! use gettextrs::TextDomain;
-//!
-//! TextDomain::new("hellorust")
-//!            .init()
-//!            .unwrap();
-//! ```
-//!
-//! [`TextDomain`]: struct.TextDomain.html
 //!
 //! ## UTF-8 is required
 //!
