4fa3e01238db8fe1a4fee71d89f6a31bd701a446
Maksym Hazevych <dpadar@protonmail.com>
2022-03-19 06:05:04 +0200

    Add dgettext
    Added dgettext macro. Extracted code creation into ToTokens implementations. Moved fallback mechanism into a separate function to reuse between macros.

--- gettext-macros/src/lib.rs
+++ gettext-macros/src/lib.rs
@@ -1,9 +1,9 @@
 #![allow(clippy::into_iter_on_ref)]
 
-use quote::quote;
+use quote::{quote, ToTokens};
 use syn::{
     parse::{Parse, ParseStream},
-    parse_macro_input, LitStr, Result, Token,
+    parse_macro_input, Expr, LitStr, Result, Token,
 };
 
 mod arguments;
@@ -11,6 +11,24 @@ mod directives;
 
 use arguments::*;
 use directives::*;
+
+fn fallback(
+    msgid: &LitStr,
+    args: &Arguments,
+    inv: proc_macro2::TokenStream,
+) -> proc_macro2::TokenStream {
+    let arguments1 = (&args.0).into_iter();
+    let arguments2 = (&args.0).into_iter();
+    quote! {{
+        match gettextrs::formatter::format(
+            &#inv,
+            &[#(#arguments1.to_string()),*]
+        ) {
+            Some(s) => s,
+            None => format!(#msgid, #(#arguments2),*)
+        }
+    }}
+}
 
 struct Invocation {
     msgid: LitStr,
@@ -31,26 +49,56 @@ impl Parse for Invocation {
     }
 }
 
-#[proc_macro]
-pub fn gettext(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input = parse_macro_input!(input as Invocation);
-    let Invocation { msgid, to_format } = input;
+impl ToTokens for Invocation {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        let msgid = &self.msgid;
+        let inv = quote! { gettextrs::gettext(#msgid) };
 
-    match to_format {
-        None => quote! { gettextrs::gettext(#msgid) }.into(),
-        Some(args) => {
-            let arguments1 = (&args.0).into_iter();
-            let arguments2 = (&args.0).into_iter();
-            quote! {{
-                match gettextrs::formatter::format(
-                    &gettextrs::gettext(#msgid),
-                    &[#(#arguments1.to_string()),*]
-                ) {
-                    Some(s) => s,
-                    None => format!(#msgid, #(#arguments2),*)
-                }
-            }}
-            .into()
+        match &self.to_format {
+            None => inv.to_tokens(tokens),
+            Some(args) => fallback(msgid, args, inv).to_tokens(tokens),
         }
     }
+}
+
+#[proc_macro]
+pub fn gettext(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    parse_macro_input!(input as Invocation)
+        .to_token_stream()
+        .into()
+}
+
+struct DInvocation {
+    d: Expr,
+    inv: Invocation,
+}
+
+impl Parse for DInvocation {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let d: Expr = input.parse()?;
+        let _ = input.parse::<Token![,]>();
+        let inv = input.parse::<Invocation>()?;
+
+        Ok(Self { d, inv })
+    }
+}
+
+impl ToTokens for DInvocation {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        let d = &self.d;
+        let msgid = &self.inv.msgid;
+        let inv = quote! { gettextrs::dgettext(#d, #msgid) };
+
+        match &self.inv.to_format {
+            None => inv.to_tokens(tokens),
+            Some(args) => fallback(msgid, args, inv).to_tokens(tokens),
+        }
+    }
+}
+
+#[proc_macro]
+pub fn dgettext(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    parse_macro_input!(input as DInvocation)
+        .to_token_stream()
+        .into()
 }
