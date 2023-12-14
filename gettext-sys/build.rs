5a3623b17dc1370cef9acd54df0736f466233458
Zhenbo Li <lizhenbo@yahoo.com>
2023-12-13 21:40:25 -0500

    add regex

--- gettext-sys/build.rs
+++ gettext-sys/build.rs
@@ -1,5 +1,6 @@
 extern crate cc;
 extern crate temp_dir;
+extern crate regex;
 
 use std::env;
 use std::ffi::OsString;
@@ -7,6 +8,7 @@ use std::fs;
 use std::io::ErrorKind;
 use std::path::{Path, PathBuf};
 use std::process::Command;
+use regex::Regex;
 use temp_dir::TempDir;
 
 fn env(name: &str) -> Option<String> {
