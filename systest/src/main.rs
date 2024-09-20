3ae09dc87d66312de98aea26a61afe46e358c24a
EDllT <47005435+EDLLT@users.noreply.github.com>
2024-09-20 15:59:35 +0300

    Fix: Use features for cross_compiling check

--- systest/src/main.rs
+++ systest/src/main.rs
@@ -1,17 +1,19 @@
+#![allow(bad_style)]
 #![allow(unused_imports)]
+extern crate gettext_sys;
 
-#[cfg(not(cross_compiling))]
+#[cfg(not(feature = "cross_compiling"))]
 use gettext_sys::*;
 
 #[cfg(not(cross_compiling))]
 include!(concat!(env!("OUT_DIR"), "/all.rs"));
 
-#[cfg(cross_compiling)]
+#[cfg(feature = "cross_compiling")]
 fn main() {
     println!("Cross-compilation detected. Skipping system tests.");
 }
 
-#[cfg(cross_compiling)]
+#[cfg(feature = "cross_compiling")]
 #[test]
 fn dummy_cross_compile_test() {
     println!("Cross-compilation detected. Skipping system tests.");
