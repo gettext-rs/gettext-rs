27c3b526e3e36aef4cba012066c99805a5832e69
王宇逸 <Strawberry_Str@hotmail.com>
2025-10-12 23:06:20 +0800

    systest: skip ptr check

--- systest/build.rs
+++ systest/build.rs
@@ -22,7 +22,7 @@ fn main() {
     // Skip ptr check because the symbol name is different between glibc
     // implementation and static lib.
     // eg. gettext is libintl_gettext in static lib
-    if env::var_os("GETTEXT_SYSTEM").is_none() || env::var("TARGET").unwrap().contains("windows") {
+    if env::var_os("GETTEXT_SYSTEM").is_none() || target.contains("windows") || target.contains("cygwin") {
         println!("Skipping ptr check");
         cfg.skip_fn_ptrcheck(|_| true);
     }
